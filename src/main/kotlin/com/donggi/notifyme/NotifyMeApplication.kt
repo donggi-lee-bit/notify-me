6cbe9dbeb17c103f9f12213524e0abad4bc31a09
donggi-lee-bit <devdonggilee@gmail.com>
2024-12-24 10:43:56 +0900

    프로젝트 init

---
+++ src/main/kotlin/com/donggi/notifyme/NotifyMeApplication.kt
@@ -0,0 +1,11 @@
+package com.donggi.notifyme
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotifyMeApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotifyMeApplication>(*args)
+}
---
+++ src/test/kotlin/com/donggi/notifyme/NotifyMeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.donggi.notifyme
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotifyMeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
